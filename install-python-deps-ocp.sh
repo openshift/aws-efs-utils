c37963def82880455af6fcda1f26b63c56a0e36b
Jan Safranek <jsafrane@redhat.com>
2025-06-19 13:39:58 +0200

    Add botocore to the final image
    Install botocore during the image build.
    
    - Install python dependencies that we have in RHEL.
    - Heavily edit requirements.txt (as requirements.txt.ocp)
      - Set version numbers to the versions available in RHEL.
      - Remove lot of test dependencies. We don't have them in RHEL and we
        won't be able to runt the unit test in our CI!
      - Set botocore version to something that works with RHEL python.
    - During the image build, use env. vars + scripts from the Red Hat build
      system and use botocore from the build system cache.
      - During any other build (e.g. in CI or on local machine), just download
        botocore from the internet.

---
+++ install-python-deps-ocp.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+# Install all efs-utils deps. Prefer RPMs, but in the end install the
+# remaining ones (= botocore) using pip.
+# Make sure pip installs from a local cache in the Red Hat build pipeline.
+# Everywhere else, let pip install from the internet.
+
+set -euxo pipefail
+
+yum update -y
+yum install --setopt=tsflags=nodocs -y nfs-utils stunnel python3 openssl util-linux which make python3-pip python3-jmespath python3-urllib3 python3-attrs python3-py python3-tomli python3-iniconfig python3-six.noarch python3-wheel
+yum clean all
+rm -rf /var/cache/yum/*
+
+REQS=/src/requirements.txt.ocp
+
+if [[ -v REMOTE_SOURCES ]]; then
+    echo "Red Hat build pipeline detected"
+    ls -lR "${REMOTE_SOURCES_DIR}/" # DEBUG
+
+    # Load cachito variables and requirements.txt.
+    # This re-configures pip to use the build system cache.
+    if [[ -d "${REMOTE_SOURCES_DIR}/cachito-gomod-with-deps" ]]; then
+        source "${REMOTE_SOURCES_DIR}/cachito-gomod-with-deps/cachito.env"
+        REQS="${REMOTE_SOURCES_DIR}/cachito-gomod-with-deps/app/requirements.txt.ocp"
+    fi
+fi
+
+# Finally, install all remaining requirements, ideally just botocore.
+# --no-build-isolation: use system setuptools and /bin/wheel from RPM packages.
+python3 -m pip install -r "${REQS}" --no-build-isolation
